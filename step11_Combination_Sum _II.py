81916059c52468efd22b6e8dec03168fa34cfaf2
Myla Prathyusha <prathyushamyla253@gmail.com>
2025-12-10 23:43:19 +0530

    implement Recursion

--- step11_Combination_Sum _II.py
+++ step11_Combination_Sum _II.py
@@ -5,3 +5,24 @@ Note: The solution set must not contain duplicate combinations.
 example:
 Input: candidates = [10,1,2,7,6,1,5], target = 8
 Output: [[1,1,6],[1,2,5],[1,7],[2,6]]'''
+def combinationSum2(candidates, target):
+    def backtrack(start, path, target):
+        if target == 0:
+            result.append(path)
+            return
+        if target < 0:
+            return
+        for i in range(start, len(candidates)):
+            if i > start and candidates[i] == candidates[i - 1]:
+                continue
+            backtrack(i + 1, path + [candidates[i]], target - candidates[i])
+
+    candidates.sort()
+    result = []
+    backtrack(0, [], target)
+    return result
+# Example usage:
+candidates = [10, 1, 2, 7, 6,
+                1, 5]   
+target = 8
+print(combinationSum2(candidates, target))
